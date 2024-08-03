9ef321606ae63de37434cbd7cd82ddfb8d9aaa1c
mousy-twirl-thrill <mousy-twirl-thrill@duck.com>
2024-08-03 18:56:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7463897729:AAHnVaZYvEpKHa7mmuU_GZdhGeEsG7PJcSI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("iamrohitchouhan")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("861027511")
 
 
 
